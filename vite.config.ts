30ad0a247b47f7103518a2fee0241b7c7b6770ed
Adekunbi Adefolarin <dekunbifola100@gmail.com>
2024-02-09 04:44:15 +0100

    test: setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths';
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [react(), viteTsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
+  server: {
+    port: 8080,
+    open: true,
+  },
+});
